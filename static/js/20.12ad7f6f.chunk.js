f582226b55deeca0d947747242de80db65aa3663
Mitch Hankins <mitch@digtix.com>
2020-11-28 00:58:45 -0800

    Updates

---
+++ static/js/20.12ad7f6f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonphyld=this.webpackJsonphyld||[]).push([[20],{563:function(e,t,a){"use strict";a.r(t);var n=a(13),s=a(154),c=a(125),r=a.n(c),i=(a(0),a(147)),o=a(129);t.default=function(){var e=Object(i.a)(),t=Object(o.useSnackbar)().enqueueSnackbar;return Object(n.jsxs)(r.a,{pageTitle:e.formatMessage({id:"toast_demo",defaultMessage:"Toast demo"}),children:[Object(n.jsx)("br",{}),Object(n.jsx)(s.a,{onClick:function(){t("Test",{variant:"info",anchorOrigin:{vertical:"top",horizontal:"center"}})},children:"OPEN Toast"})]})}}}]);
+//# sourceMappingURL=20.12ad7f6f.chunk.js.map
